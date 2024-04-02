6ae1685873cbabf6e5209a546625ae1c7494f385
Jay <jayoshimi31@gmail.com>
2024-04-02 01:12:46 -1000

    making functionaltest

--- functional_tests.py
+++ functional_tests.py
@@ -3,7 +3,11 @@ from selenium.webdriver.firefox.options import Options
 
 options = Options()
 options.binary_location = r"C:\Program Files\Mozilla Firefox\Firefox.exe"
+
+# Edith has heard about a cool new online to-do app. She goes
+# to check its home page
 browser = webdriver.Firefox(options=options)
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title and header mention to-do lists
+assert 'To-Do' in browser.title
